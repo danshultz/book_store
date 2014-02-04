b791284c464569b2a00a33c838c2d8ab7c110ca0
bahrens <ahrensben@gmail.com>
2014-02-04 16:14:27 -0500

    add devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :timeoutable and :omniauthable
+  devise(
+    :confirmable,
+    :database_authenticatable,
+    :lockable,
+    :recoverable,
+    :registerable,
+    :rememberable,
+    :trackable,
+    :validatable
+  )
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 BookStore::Application.routes.draw do
+  devise_for :users
   root :to => "home#index"
 end
